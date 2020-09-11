10ea5e82ece86c848e26675a5673775ded0e42db
mvohra447 <54878239+mvohra447@users.noreply.github.com>
2020-09-11 12:05:31 +0530

    Update and rename provider.tf to variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,20 @@
+variable "customer" {
+}
+
+variable "project" {
+}
+
+variable "env" {
+}
+
+variable "AZURE_SUBSCRIPTION_ID" {
+}
+
+variable "AZURE_CLIENT_ID" {
+}
+
+variable "AZURE_CLIENT_SECRET" {
+}
+
+variable "AZURE_TENENT_ID" {
+}
