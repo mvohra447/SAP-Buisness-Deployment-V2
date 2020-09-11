afd6fc20f7bbd460808d09c6152c2a9d31c46c5c
mvohra447 <54878239+mvohra447@users.noreply.github.com>
2020-09-11 12:06:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  version = "=1.36.1"
+  subscription_id = var.AZURE_SUBSCRIPTION_ID
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
+  tenant_id       = var.AZURE_TENENT_ID
+}
+
 module "sap-function" {
   #####################################
   # Do not modify the following lines #
